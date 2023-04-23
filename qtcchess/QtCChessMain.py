dda8f5dcd334495fedfb6d34c79652d7a86a39e4
Walker Li <walker8088@gmail.com>
2023-04-23 17:40:26 +0800

    bug fix

--- qtcchess/QtCChessMain.py
+++ qtcchess/QtCChessMain.py
@@ -351,8 +351,8 @@ class MainWindow(QMainWindow):
     def center(self):
         screen = QDesktopWidget().screenGeometry()
         size = self.geometry()
-        self.move((screen.width() - size.width()) / 2,
-                  (screen.height() - size.height()) / 2)
+        self.move((screen.width() - size.width()) // 2,
+                  (screen.height() - size.height()) // 2)
 
     def closeEvent(self, event):
         self.writeSettings()
